6b9427835cae9a40e61baaf9e519599cc59a7cc8
Yusup Velimetov <yusupvelimetov@mail.ru>
2024-04-07 14:37:38 +0300

    123

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #if defined(_WIN32)
+
 #ifndef _WIN32_WINNT
 #define _WIN32_WINNT 0x0600
 #endif
@@ -6,7 +7,12 @@
 #include <ws2tcpip.h>
 #pragma comment (lib,"ws2_32.lib")
 
+#define ISVALIDSOCKET(s) ((s)!=INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
 #else
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet.h>
@@ -14,6 +20,11 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <errno.h>
+
+#define SOCKET int
+#define ISVALIDSOCKET(s) ((s)>=0)
+#define CLOSESOCKET(s) close(s)
+#define GETSOCKETERRNO() (errno)
 #endif
 
 #include <stdio.h>
