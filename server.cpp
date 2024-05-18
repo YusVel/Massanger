6ef05c88cc2f3b0eff415469e530e0266178db4c
Yusup Velimetov <yusupvelimetov@mail.ru>
2024-05-19 00:02:50 +0300

    123
    123

--- server.cpp
+++ server.cpp
@@ -12,7 +12,7 @@ int main()
 	WSADATA d;
 	if (WSAStartup(MAKEWORD(2, 2), &d))
 	{
-		fprintf(stderr, "FAILD to INITIASLIZE!!!\n");
+		fprintf(stderr, "##### FAILD to INITIASLIZE!!! #####\n");
 		printf("%d", WSAGetLastError());
 		return 1;
 	}
@@ -21,7 +21,7 @@ int main()
 
 
 ////////////////////////////////////////////////////////////
-printf("Configuring local server address...\n");
+printf("***** Configuring local server address...\n");
 struct addrinfo tips;  //Настройки адреса сервера
 memset(&tips,0,sizeof(tips));
 tips.ai_family = AF_INET6;
@@ -39,24 +39,24 @@ getaddrinfo(serveraddress, serverport, &tips, &bind_address); //заполнение струк
 
 if (getnameinfo(bind_address->ai_addr, bind_address->ai_addrlen, serveraddress, ADDRLEN, serverport, PORTLEN, NI_NUMERICHOST)) //получаем адрес из сокета
 {
-	fprintf(stderr, "Error getnameinfo() (%d)", GETSOCKETERRNO());
+	fprintf(stderr, "##### Error getnameinfo() (%d) #####", GETSOCKETERRNO());
 	show_error(GETSOCKETERRNO());
 	return 1;
 }
-printf("SERVER ADDRESS %s:%s\n", serveraddress, serverport);
+printf("***** SERVER ADDRESS %s:%s\n", serveraddress, serverport);
 
-printf("Creating socet...\n");
+printf("***** Creating socet... \n");
 SOCKET server_sock;
 server_sock = socket(bind_address->ai_family,bind_address->ai_socktype,bind_address->ai_protocol); //создаем серверный сокет, через который сервер будет общатся с клиентами
  if(!ISVALIDSOCKET(server_sock))
  {
-	 fprintf(stderr,"Creating server socket FAILED! (%d)\n",GETSOCKETERRNO());
+	 fprintf(stderr,"##### Creating server socket FAILED! (%d) #####\n",GETSOCKETERRNO());
 	 show_error(GETSOCKETERRNO());
 	 return 1;
  }
  else
  {
-	 printf("Server socket is created!\n");
+	 printf("***** Server socket is created!\n");
  }
  
 
@@ -64,37 +64,37 @@ server_sock = socket(bind_address->ai_family,bind_address->ai_socktype,bind_addr
 #if defined(_WIN32)
  if (setsockopt(server_sock, IPPROTO_IPV6, IPV6_V6ONLY, (char*)&option, sizeof(option))) //функция изеняющая настрой ки сокета, для приема клиентов с адресами как IPV4  так и IPV^6
  {
-	 fprintf(stderr, "setsockopt() faild!!! (%d)", GETSOCKETERRNO());
+	 fprintf(stderr, "##### setsockopt() faild!!! (%d) ##### ", GETSOCKETERRNO());
 	 show_error(GETSOCKETERRNO());
 	 return 1;
  }
 #else
  if (setsockopt(server_sock, IPPROTO_IPV6, IPV6_V6ONLY, (void*)&option, sizeof(option)))
  {
-	 fprintf(stderr, "setsockopt() faild!!! (%d)", GETSOCKETERRNO());
+	 fprintf(stderr, "##### setsockopt() faild!!! (%d) #####", GETSOCKETERRNO());
 	 perror("ERROR");
 	 return 1;
  }
 #endif
  if(bind(server_sock,bind_address->ai_addr,bind_address->ai_addrlen)) // настраиваем сокет, присваиваем ему адрес
  {
-	 fprintf(stderr,"bind() FAILED! (%d)\n",GETSOCKETERRNO());
+	 fprintf(stderr,"##### bind() FAILED! (%d) #####\n",GETSOCKETERRNO());
 	 perror("ERROR");
 	 return 1;
  }
   else
  {
-	 printf("Server's address is bound!\n");
+	 printf("***** Server's address is bound!\n");
  }
  freeaddrinfo(bind_address); // высвобождаем память занятую структурой адреса 
- printf("Listening....\n");
- if(listen(server_sock,10)<0)
+ printf("***** Listening....\n");
+ if(listen(server_sock,MAXCLIENTS)<0)
  {
-	 fprintf(stderr,"linten() FAILED! (%d)\n",GETSOCKETERRNO());
+	 fprintf(stderr,"##### linten() FAILED! (%d) #####\n",GETSOCKETERRNO());
 	 perror("ERROR");
 	 return 1;
  }
- printf("Waiting for connection....\n");
+ printf("***** Waiting for connection.... \n");
  
  //////////////////////////
  
@@ -103,8 +103,19 @@ server_sock = socket(bind_address->ai_family,bind_address->ai_socktype,bind_addr
  FD_SET(server_sock,&sockets_set); //добавляем слушающий сокет
  SOCKET max_socket = server_sock; // создаем меременную max_socket для хранения максимального номера файлового дескриптора
  
+ /*
+ SOCKET *arr = (SOCKET*)malloc(sizeof(SOCKET) * MAXCLIENTS); //массив сокетов клиентов создается для отправки сообщения всем клиентам
+ for (int i = 0; i < MAXCLIENTS; i++)						//инициализируем массив нулями. 0(stdin отслеживается селектом и _kbhit())
+ {
+	 arr[i] = 0;
+ }*/
  
- 
+ SOCKET arr[MAXCLIENTS] = { 0 };
+
+
+ double x = 0, y = 0;  //переменные для калькуляции
+ char ch = '\0';   //математическое действие.
+
  while(1)
  {
 	 fd_set read_sockets; //создаем набор сокетов, которые будут проверятся на входящие данные. И инициализируем его нашим набором.
@@ -113,7 +124,7 @@ server_sock = socket(bind_address->ai_family,bind_address->ai_socktype,bind_addr
 	 
 	 if(select(max_socket+1,&read_sockets, 0,0,0)<0) // с помощью функции select проверяем все сокеты на предмет воходящих данных. 
 	 {
-		 fprintf(stderr,"select() faild(%d)/n",GETSOCKETERRNO());
+		 fprintf(stderr,"##### select() faild(%d) #####\n",GETSOCKETERRNO());
 		 show_error(GETSOCKETERRNO());
 		 return 1;
 	 }
@@ -122,18 +133,17 @@ server_sock = socket(bind_address->ai_family,bind_address->ai_socktype,bind_addr
 	 {
 		 if(FD_ISSET(i,&read_sockets)) // если select отловил сокет с входящими данными
 		 {
+			 char adr[ADDRLEN] = { 0 };
+			 char client_name[ADDRLEN] = { 0 };
+
 			 if(i==server_sock) // если это слушающий сокет
 			 {
-				 
-				 char adr[ADDRLEN] = {0};
-				 char client_name[ADDRLEN] = {0};
-				 
 				 struct sockaddr_storage client_address; //новый адрес входящего клиента.
 				 socklen_t client_len = sizeof(client_address);
 				 SOCKET new_client = accept(server_sock, (struct sockaddr*)&client_address, &client_len); //принимаем нового клиента
 				 if(!ISVALIDSOCKET(new_client))
 				 {
-					  fprintf(stderr,"accept() new client faild(%d)",GETSOCKETERRNO());
+					  fprintf(stderr,"##### accept() new client faild(%d) #####",GETSOCKETERRNO());
 					  show_error(GETSOCKETERRNO());
 					  return 1;
 				 }
@@ -141,57 +151,63 @@ server_sock = socket(bind_address->ai_family,bind_address->ai_socktype,bind_addr
 				 {
 					 if(getnameinfo((struct sockaddr*)&client_address,client_len,adr,ADDRLEN,0,0,NI_NUMERICHOST)!=0) //получаем адрес нового клиента
 					 {
-						 fprintf(stderr,"getnameinfo() faild(%d)",GETSOCKETERRNO());
+						 fprintf(stderr,"###### getnameinfo() faild(%d) #####",GETSOCKETERRNO());
 						 show_error(GETSOCKETERRNO());
 						 return 1;
 					 }
 					 
 					 if(getnameinfo((struct sockaddr*)&client_address,client_len,client_name,ADDRLEN,0,0,1)!=0) //получаем имя ПК нового клиента
 					 {
-						 fprintf(stderr,"getnameinfo() faild(%d)",GETSOCKETERRNO());
+						 fprintf(stderr,"###### getnameinfo() faild(%d) #####",GETSOCKETERRNO());
 						 show_error(GETSOCKETERRNO());
 						 return 1;
 					 }
-					 printf("Accept new(%d) client: %s (%s)\n",new_client,client_name,adr);
+					 printf("***** Accept new(%d) client: %s (%s)\n", (int)new_client,client_name,adr);
 					 FD_SET(new_client,&sockets_set);
 					 if(max_socket<new_client)
 					 {
 						 max_socket = new_client;
 					 }
+
+					 add_client_to_arr(arr, MAXCLIENTS, new_client); //добавляем сокет в массив, для отправки всем сообщений от севера
 				 } 
 			 }
 			 else
 			 {
-				 
-				char adr[ADDRLEN] = "123456789";
 				char MSG[MSGSIZE]= {0};
 				int recv_bytes = recv(i,MSG,MSGSIZE,0);
 				//printf("Recieve %d bytes: %.*s\n",recv_bytes,recv_bytes,MSG);
 				if(recv_bytes<1)
 				{
+#if !defined(_WIN32)
 					struct sockaddr_storage lost_client;
 					memset(&lost_client,0,sizeof(lost_client));
 					socklen_t lost_client_len = sizeof(struct sockaddr_in);
-
-					
 					if(getpeername(i,(struct sockaddr*)&lost_client,&lost_client_len)!=0)
 					{
-						fprintf(stderr,"getnameinfo().lost_client adr. faild(%d)",GETSOCKETERRNO());
+						fprintf(stderr,"getpeername().lost_client adr. faild(%d)",GETSOCKETERRNO());
 						show_error(GETSOCKETERRNO());
 						return 1;
 					}
-					
 					if(getnameinfo((struct sockaddr*)&lost_client,lost_client_len,adr,sizeof(adr),0,0,NI_NUMERICHOST)!=0)
 					{
-						 fprintf(stderr,"getnameinfo() loose client. faild(%d)",GETSOCKETERRNO());
+						 fprintf(stderr,"getnameinfo() loose client faild(%d)",GETSOCKETERRNO());
 						 show_error(GETSOCKETERRNO());
 						 return 1;
 					}
-					
+					if (getnameinfo((struct sockaddr*)&post_client, post_client_len, client_name, sizeof(client_name), 0, 0, 1) != 0)
+					{
+						fprintf(stderr, "##### getnameinfo() loose client. faild(%d) #####", GETSOCKETERRNO());
+						show_error(GETSOCKETERRNO());
+						return 1;
+					}
+#endif
 					FD_CLR(i,&sockets_set);
 					CLOSESOCKET(i);
-					printf("WE lose client(socket=%d):(%s)\n",i,adr);
+					printf("***** WE lost client(socket=%d): %s (%s)\n",(int)i, client_name,adr);
+					del_client_from_arr(arr, MAXCLIENTS, i); //удаляем сокет из массива, клиент выбыл и сообщения от сервера ему более не доступны.
 					continue;
+
 				}
 				else
 				{
@@ -201,18 +217,46 @@ server_sock = socket(bind_address->ai_family,bind_address->ai_socktype,bind_addr
 
 					if(getpeername(i,(struct sockaddr*)&post_client,&post_client_len)!=0)
 					{
-						fprintf(stderr,"getnameinfo().post_client adr. faild(%d)",GETSOCKETERRNO());
+						fprintf(stderr,"##### getperrname().post_client adr. faild(%d) #####",GETSOCKETERRNO());
 						show_error(GETSOCKETERRNO());
 						return 1;
 					}
 					if(getnameinfo((struct sockaddr*)&post_client,post_client_len,adr,sizeof(adr),0,0,NI_NUMERICHOST)!=0)
 					{
-						 fprintf(stderr,"getnameinfo() loose client. faild(%d)",GETSOCKETERRNO());
+						 fprintf(stderr,"##### getnameinfo() loose client adr. faild(%d) #####",GETSOCKETERRNO());
 						 show_error(GETSOCKETERRNO());
 						 return 1;
 					}
+					if (getnameinfo((struct sockaddr*)&post_client, post_client_len, client_name, sizeof(client_name), 0, 0, 1) != 0)
+					{
+						fprintf(stderr, "##### getnameinfo() loose client. faild(%d) #####", GETSOCKETERRNO());
+						show_error(GETSOCKETERRNO());
+						return 1;
+					}
 
-					printf("Recieve from(socket=%d) (%s): %.*s\n",i,adr,recv_bytes,MSG);
+					/////////////////////////////////
+
+					////////////////////////////////
+
+
+					printf("Recieve from(socket=%d) %s (%s): %.*s\n", (int)i, client_name,adr,recv_bytes,MSG);
+
+					//show_arr(arr, MAXCLIENTS);
+
+					for (int j = 0; j < MAXCLIENTS; j++)
+					{
+						if (arr[j] != 0)
+						{
+							int send_bytes = 0;
+							if(send_bytes = send(arr[j], MSG, strlen(MSG), 0) < 1)
+							{
+								fprintf(stderr, "##### send() to socket(%d) faild(%d) #####", (int)arr[j], GETSOCKETERRNO());
+								show_error(GETSOCKETERRNO());
+								return 1;
+							}
+							printf("SEND(%d bytes) to (socket=%d) %s (%s): %.*s\n", send_bytes, (int)arr[j], client_name, adr, send_bytes, MSG);
+						}
+					}
 				}
 			 }
 		 }
@@ -291,8 +335,8 @@ printf("Client(socket=%d) conected from  %s\n",client_sock,address);
   CLOSESOCKET(client_sock);
   */
   
-  
-  CLOSESOCKET(server_sock);
+// free(arr);
+ CLOSESOCKET(server_sock);
 #if defined(_WIN32)
 	WSACleanup();
 #endif
